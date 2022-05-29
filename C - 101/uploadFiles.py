ebf9abd9a649c5eba1c470c665ad8719c73d73be
KingAgastya <63898735+KingAgastya@users.noreply.github.com>
2022-05-29 10:01:45 +0530

    Update uploadFiles.py

--- C - 101/uploadFiles.py
+++ C - 101/uploadFiles.py
@@ -1,4 +1,6 @@
+from threading import local
 import dropbox
+import os
 
 class TransferData:
     def __init__(self, access_token):
@@ -7,9 +9,14 @@ class TransferData:
     def upload_file(self, file_from, file_to):
         dbx = dropbox.Dropbox(self.access_token)
 
+        for root, dirs, files in os.walk(file_from):
+            for fileName in files:
+                local_path = os.path.join(root, fileName)
+                relative_path = os.path.relpath(local_path, file_from)
+                dropbox_path = os.path.join(file_to, relative_path)
 
-        f = open(file_from, 'rb')
-        dbx.files_upload(f.read(), file_to)
+                with open(local_path, 'rb') as f:
+                    dbx.files_upload(f.read(), dropbox_path, mode = WriteMode('overwrite'))
 
 def main():
     access_token = 'sl.BA3gHXDGkDTeJKXeVJDbrFV2I4BFsebr0MZaVDD7DzUrqhj-hX8NJJv8YYrdotsreEBUtZ-QvsONqi3xhy94gcgFSIUgYb_-t6Hs4lrxb-1jLCDJmkV_zgewvOlh92QjO5_wjmc'
